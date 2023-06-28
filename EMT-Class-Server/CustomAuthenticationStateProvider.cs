d5ea462f37dfd0a3d4237e54a80215709905e0f2
Logan Hylton <lhyltoncs@gmail.com>
2023-06-28 13:31:11 -0400

    first commit

---
+++ EMT-Class-Server/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+
+namespace EMT_Class_Server
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var identity = new ClaimsIdentity();
+            var user = new ClaimsPrincipal(identity);
+
+            return Task.FromResult(new AuthenticationState(user));
+        }
+
+        public void AuthenticateUser(string email, string password)
+        {
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, email),
+            }, "Custom Authentication");
+
+            var user = new ClaimsPrincipal(identity);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+
+        public void Signup(string email, string password)
+        {
+            PasswordHasher<string> hasher = new PasswordHasher<string>();
+            var hashedPassword = hasher.HashPassword(email, password);
+
+
+        }
+    }
+}
